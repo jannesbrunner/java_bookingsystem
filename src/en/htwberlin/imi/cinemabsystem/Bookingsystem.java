cf96750214545da44e5a95f896f808705aa1e929
jannesbrunner <jannes.brunner@student.htw-berlin.de>
2016-04-19 10:20:42 +0200

    Add native Bookingsystem class

---
+++ src/en/htwberlin/imi/cinemabsystem/Bookingsystem.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package en.htwberlin.imi.cinemabsystem;
+
+/**
+ * @author Jannes Brunner, Mario Schuetz, Katharina Wunder
+ *
+ */
+public class Bookingsystem {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
