472ef8b32f3263062e62a96e2d7f15f90ed1a5a1
Mario.Schuetz <mayo2@gmx.de>
2016-04-24 22:07:32 +0200

    refactored book method

--- src/de/htwberlin/imi/cinemabsystem/Storage.java
+++ src/de/htwberlin/imi/cinemabsystem/Storage.java
@@ -161,7 +161,9 @@ public class Storage {
 			Scanner input = new Scanner(System.in);
 			System.out.println("How many seats would you like to book? ");
 			int seats = input.nextInt();
-			inLine(seats);			
+			if(seats > 1){
+				inLine(seats);
+			}
 		}
 		else{
 			System.out.println("Not a valid choice.");
@@ -171,11 +173,10 @@ public class Storage {
 	public int inLine(int seatQuantity){
 		int inLine = 1;
 		
-		if(seatQuantity > 1){
-			Scanner input = new Scanner(System.in);
-			System.out.println("Would you like to choose " + seatQuantity + " in a row (1) or separated (2)? ");
-			inLine = input.nextInt();
-		}
+		Scanner input = new Scanner(System.in);
+		System.out.println("Would you like to choose " + seatQuantity + " in a row (1) or separated (2)? ");
+		inLine = input.nextInt();
+	
 		return inLine;
 	}
 }
